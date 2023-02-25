7a7424d3359e63e3ab1a882807576194c7517210
Monu kumar <120436973+7060monu@users.noreply.github.com>
2023-02-26 00:45:24 +0530

    Add files via upload

---
+++ sumevenodd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int num,i,sum=0,temp;
+    printf("enter num");
+    scanf("%d",&num);
+    for(i==0;i<=num;i++){
+        if(i%2==0){
+        sum=sum+i;
+        printf("sum of even is%d",sum);
+        }
+        else
+        temp=temp+i;
+        printf("sum of odd is",temp);
+}
+    }
+}
