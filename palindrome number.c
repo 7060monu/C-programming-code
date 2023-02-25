f66bb63cab937f36e0bae78103ea201cdf063bbf
7060monu <120436973+7060monu@users.noreply.github.com>
2023-02-26 00:18:17 +0530

    Update palindrome number.c

--- palindrome number.c
+++ palindrome number.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main(){
-    int num,rem,rev=0;
+    int num,rem,rev=0,temp=num;
     printf("enter the num");
     scanf("%d",&num);
     while(num!=0){
         rem=num%10;
-        num=num/10;
         rev=rev*10+rem;
+        num=num\10;
     }
     printf("%d",rev);
-    if(rev=num)
+    if(temp = rev)
     printf(" num is palindrome");
     else
     printf("num is not palindrome");
