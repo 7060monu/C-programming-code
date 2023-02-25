611eef4dbe772062bfa5ceb08b5ee52dff028fb6
7060monu <120436973+7060monu@users.noreply.github.com>
2023-02-26 00:10:48 +0530

    Add files via upload

---
+++ sumeven.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int num,i,sum=0;
+    printf("enter number");
+    scanf("%d",&num);
+    for(i=0;i<=num;i++){
+        if(i%2==0){
+            sum=sum+i;
+        }
+    }
+    printf("sum is%d",sum);
+}
