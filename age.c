3c1ed63d3f3fc2cc115d09f0897129a5be810dfe
Monu kumar <120436973+7060monu@users.noreply.github.com>
2023-02-26 17:45:17 +0530

    Add files via upload

---
+++ age.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int age;
+    printf("enter age");
+    scanf("%d",age);
+    if(age>20 && age<30){
+        printf("you are young");
+    } 
+    else{
+        print("enter valid age");  
+    }
+}
