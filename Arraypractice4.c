d7a62b9b6ef8e2439b83a14a8186c1096882d1d8
Monu kumar <120436973+7060monu@users.noreply.github.com>
2023-03-02 09:16:50 +0530

    Add files via upload

---
+++ Arraypractice4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main(){
+    int a[5],i;
+    int max[4]= {0};
+    printf(" enter the element :");
+    for(i=0;i<5;i++){
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<5;i++){
+        if(a[i]<max[4]){
+            max[4]=a[i];
+        }
+    }
+    printf("%d",max[4]);
+}
