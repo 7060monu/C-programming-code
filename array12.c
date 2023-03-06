942b75bcc2fa5b08a4ad87fcba271743805dd1e0
Monu kumar <120436973+7060monu@users.noreply.github.com>
2023-03-06 14:27:57 +0530

    Add files via upload

---
+++ array12.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main(){
+    int n,a[100] = {},pos,i;
+    printf(" enter the value n");
+    scanf("%d",&n);
+    printf(" enter the array of element");
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    printf(" postion is :");
+    scanf("%d",&pos);
+    for(i=pos-1;i<n-1;i++){
+        a[i] = a[i+1];
+        
+    }
+    for(i=0;i<n-1;i++){
+        printf("%d",a[i]);
+    }
+}
+
